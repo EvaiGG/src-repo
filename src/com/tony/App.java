eee2e78c4c89a5c05c94845117ea93b6d1445289
ctl <1415945977@qq.com>
2024-10-02 21:07:26 +0800

    fork-app-run

--- src/com/tony/App.java
+++ src/com/tony/App.java
@@ -1,7 +1,13 @@
 package src.com.tony;
 
-public class App {
+public class App implements Runnable{
     public static void main(String[] args) {
         System.out.println("hello");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("hello world");
+
     }
 }
